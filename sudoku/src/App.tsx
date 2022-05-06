d664a8207afb16dae0fca7ceceb01dc966f03844
Joey Muller <jmullercuber@gmail.com>
2022-05-05 20:05:12 -0600

    Added header, line and started the outline to the table.

--- sudoku/src/App.tsx
+++ sudoku/src/App.tsx
@@ -1,24 +1,39 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+    <div className="home">
+      <header className="homeHeader">
+        <h1 >Sudoku</h1>
+        <hr className="line"></hr>
       </header>
+      <table className="table">
+        <thead>
+          <tr>
+            <th> Row 1</th>
+            <th> Row 2 </th>
+            <th> Row 3 </th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr>
+            <td>1, 1</td>
+            <td>1, 2</td>
+            <td>1, 3</td>
+          </tr>
+          <tr>
+            <td>2, 1</td>
+            <td>2, 2</td>
+            <td>2, 3</td>
+          </tr>
+          <tr>
+            <td>3, 1</td>
+            <td>3, 2</td>
+            <td>3, 3</td>
+          </tr>
+        </tbody>
+      </table>
     </div>
   );
 }
